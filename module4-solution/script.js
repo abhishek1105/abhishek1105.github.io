ecab8ab1880a37a89ab2c3575f9772ecd905fde7
ABHISHEK PATHAK <abhishek.pathak1105@gmail.com>
2022-06-22 13:53:21 +0530

    Update script

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -12,7 +12,7 @@ names[9] = "Jim";
 
 for(var i=0 ; i<names.length; i++) {
 	if(names[i].charAt(0)=='J' || names[i].charAt(0)=='j'){
-		console.log("Goodbye "+names[i])
+		console.log("Good Bye "+names[i])
 	}
 	else{
 		console.log("Hello "+names[i])
